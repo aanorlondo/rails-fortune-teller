1bcacb68ce87986b59bed5d46d0b6c6661a70b63
aanorlondo <khaled.khebbeb@gmail.com>
2024-04-25 09:50:14 +0200

    feat: prepare to serve behind nginx on rasp4

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 CWD="$(dirname "$(readlink -f "$0")")" || exit 1
-host_ip=$(ipconfig getifaddr en0)
+host_ip=$(hostname -I | awk '{print $1}')
 
 # clean
 docker rm -f FORTUNE-TELLER-LOCAL
 
 # build
-docker build -t fortune_teller:local .
+docker build -t fortune_teller:rasp4 .
 
 # run
 docker run \
@@ -15,4 +15,4 @@ docker run \
     --env RAILS_MASTER_KEY="$(cat $CWD/config/master.key)" \
     --add-host="host:${host_ip}" \
     --name FORTUNE-TELLER-LOCAL \
-    fortune_teller:local
+    fortune_teller:rasp4
